a87271708c147e96f41a4c21260f8949e144b044
Nimalit <105597685+Nimalit@users.noreply.github.com>
2022-12-06 14:27:03 +0300

    HW_1.2
    HW_1.2

--- appmessage/src/components/Message/message.js
+++ appmessage/src/components/Message/message.js
@@ -13,14 +13,16 @@ export const Message = ({ name, age }) => {
         setAge(event.target.value)
     }
     return (
-        <div className="Basic">
-            <h1 className="headerBasic">Добро пожаловать мой первый Message</h1>
-            <p className="dataInput">Укажите ваше имя: {yourName}</p>
-            <input type="text" className="Field" onChange={handleChangeName} />
-            <p className="dataInput">Мой возвраст: {yourAge} года</p>
-            <input type="text" className="Field" onChange={handleChangeAge} />
-            <h3 className="message"> Меня зовут, {name},возвраст {age}
-            </h3>
-        </div>
+        <>
+            <div className="Basic">
+                <h1 className="headerBasic">Добро пожаловать мой первый Message</h1>
+                <p className="dataInput">Укажите ваше имя: {yourName}</p>
+                <input type="text" className="Field" onChange={handleChangeName} />
+                <p className="dataInput">Мой возвраст: {yourAge} </p>
+                <input type="text" className="Field" onChange={handleChangeAge} />
+                <h3 className="message"> Меня зовут, {name},возвраст {age}
+                </h3>
+            </div>
+        </>
     );
 }; 
